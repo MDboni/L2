0f5271f02d599f95ef824f0fe192836671d7cc64
Boni Amin Jayed <aminboni048@gmail.com>
2026-05-03 10:29:43 +0600

    Add TypeScript interfaces for User, Order, and Employee, along with payment handling function

---
+++ mod1/src/mod2/v1InterFace.ts
@@ -0,0 +1,104 @@
+interface User {
+    name: string;
+    age: number;
+    email: string;
+}
+
+const abc: User = {
+    name: "Boni Amin jayed",
+    age: 25,
+    email: "boni.amin.jayed@example.com"
+}
+
+console.log(abc);
+
+type UserType = {
+    name: string;
+    age: number;
+    email: string;
+}
+
+const a: UserType = {
+    name: "Boni Amin jayed",
+    age: 25,
+    email: "dw"
+}
+console.log(a);
+
+
+interface order {
+   product : string;
+   price : number;
+   quantity : number;
+   status : "pending" | "shipped" | "delivered";
+}
+
+const or : order[] = [
+   {
+      product: "Laptop",
+      price: 1000,
+      quantity: 1,
+      status: "pending"
+   },
+   {
+      product: "Phone",
+      price: 500,
+      quantity: 2,
+      status: "shipped"
+   },
+   {
+      product: "Tablet",
+      price: 300,
+      quantity: 1,
+      status: "delivered"
+   }
+]
+
+console.log(or);
+
+
+interface Employee {
+    id: number;
+    name: string;
+    department: string;
+    salary: number;
+}
+
+const ad = (employee: Employee): void =>{
+      console.log(`Employee ID: ${employee.id}`);
+      console.log(`Employee Name: ${employee.name}`);
+      console.log(`Employee Department: ${employee.department}`);
+      console.log(`Employee Salary: ${employee.salary}`);
+}
+ad({
+    id: 1,
+    name: "Boni Amin jayed",
+    department: "IT",
+    salary: 50000
+})
+
+console.log(ad);
+
+type paymentResult = {
+      success: boolean;
+      message: string;
+      amount: number;
+}
+
+const paye = (am : paymentResult): void =>{
+   if(am.amount > 0){
+      console.log(`paayment amount:${am.amount}is${am.message}`);
+      
+   }
+}
+
+paye({
+   success: true,
+   message: "successful",
+   amount: 100
+})
+
+console.log(paye);
+
+
+
