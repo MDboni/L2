91550a33e493d1a992a70fe46aabb0134c08b5cd
Boni Amin Jayed <aminboni048@gmail.com>
2026-05-10 17:40:37 +0600

    Refactor arrayIsArray.ts to include additional examples and improve clarity

--- mod1/src/mod3/arrayIsArray.ts
+++ mod1/src/mod3/arrayIsArray.ts
@@ -8,3 +8,20 @@ const b : object = { name: "John", age: 30 };
 
 const c = Array.isArray(b); 
 console.log(c); // Output: falses
+
+
+const res = {
+    status: "200",
+    data : {
+        product : [
+            { id: 1, name: "Product 1", price: 10 },
+            { id: 2, name: "Product 2", price: 20 },
+            { id: 3, name: "Product 3", price: 30 }
+        ]
+    }
+}
+
+res.data.product.map((p)=> console.log(p.name));
+
+const d = Array.isArray(res.data.product);
+console.log(d); // Output: true
