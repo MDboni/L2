c87c58c3d493c50dbec6df1dda2f4c1a5fa70c0e
Boni Amin Jayed <aminboni048@gmail.com>
2026-05-10 17:13:53 +0600

    Add check for object type in arrayIsArray implementation

--- mod1/src/mod3/arrayIsArray.ts
+++ mod1/src/mod3/arrayIsArray.ts
@@ -3,3 +3,8 @@ const abc : number[] = [1, 2, 3, 4, 5];
 const a = Array.isArray(abc);
 
 console.log(a); 
+
+const b : object = { name: "John", age: 30 };
+
+const c = Array.isArray(b); 
+console.log(c); // Output: falses
